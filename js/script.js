8506f36059d53c31605643a33a6714bd7c7afab3
Vasu Beachoo <vasubeachoo@gmail.com>
2022-05-07 21:40:19 -0400

    Add error icon

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const formContainer = document.querySelector(".form-container");
 const emailForm = document.querySelector(".email-form");
 const emailInput = document.querySelector(".email-input");
 const errorMsgContainer = document.querySelector(".error-msg-container");
@@ -9,15 +10,26 @@ function displayErrorMsg() {
   errorMsgContainer.append(errorMsg);
 }
 
+const errorIcon = document.createElement("img");
+errorIcon.src = "./images/icon-error.svg";
+errorIcon.alt = "icon-error";
+errorIcon.className = "error-icon";
+
+function displayErrorIcon() {
+  formContainer.append(errorIcon);
+}
+
 emailForm.addEventListener("submit", (e) => {
   if (emailInput.value === "") {
     errorMsg.innerText = "Uh oh! It looks like you forgot to add your email";
     e.preventDefault();
     displayErrorMsg();
+    displayErrorIcon();
   }
   if (!emailInput.validity.valid) {
     errorMsg.innerText = "Please provide a valid email";
     e.preventDefault();
     displayErrorMsg();
+    displayErrorIcon();
   }
 });
